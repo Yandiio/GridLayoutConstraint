27799fe056ab37e3fa3735218e635bdf6be1f69f
yandiio <yandifenanda95@gmail.com>
2019-07-25 16:51:35 +0700

    Initial commit

---
+++ app/src/main/java/com/pwpb/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pwpb.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_test);
+    }
+}
